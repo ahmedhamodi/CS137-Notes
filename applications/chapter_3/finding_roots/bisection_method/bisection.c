437bb81f13f3a1258bf7a4aa95b4d20407a45250
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-10-06 16:04:52 -0400

    Updated bisection method to work for multiple functions.

--- applications/chapter_3/finding_roots/bisection_method/bisection.c
+++ applications/chapter_3/finding_roots/bisection_method/bisection.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include "bisection.h"
 
-double f(double x){return x-cos(x);}
+// double f(double x){return x-cos(x);}
 
-double bisect(double a, double b, double epsilon, int iterations) {
+double bisect(double a, double b, double epsilon, int iterations, double (*f)(double)) {
 	double m = a, fm;
 	assert (epsilon > 0.0 && f(a)*f(b) < 0);
 	for (int i = 0; i < iterations; i++) {
@@ -19,6 +19,11 @@ double bisect(double a, double b, double epsilon, int iterations) {
 	return m;
 }
 
+double g(double x){return x-cos(x);}
+double h(double x){return x*x*x-x+1;}
+
 int main(void) {
-	printf("%g\n", bisect(-10, 10, 0.0001, 50));
+	printf("%g\n", bisect(-10, 10, 0.0001, 50, g));
+	printf("%g\n", bisect(-10, 10, 0.0001, 50, h));
+	return 0;
 }
--- applications/chapter_3/finding_roots/bisection_method/bisection.h
+++ applications/chapter_3/finding_roots/bisection_method/bisection.h
@@ -6,7 +6,7 @@ Pre: none
 Post: returns the value of x - cos(x)
 */
 
-double f(double x);
+// double f(double x);
 
 /*
 Pre: epsilon > 0 is a toleratnce. iterations > 0,
@@ -15,6 +15,6 @@ Post: Returns an approximate root for f(x) using
 	bisection method. Stops when either number of
 	iterations is exceeded or |f(m)| < epsilon
 */
-double bisect(double a, double b, double epsilon, int iterations);
+double bisect(double a, double b, double epsilon, int iterations, double (*f)(double));
 
 #endif
