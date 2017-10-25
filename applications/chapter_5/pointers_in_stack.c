360059c97e8bd26e6fe2c1028e143e95fe8b8729
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-10-25 14:02:24 -0400

    Started chapter 5. Created an application that discusses pointers in stack.

---
+++ applications/chapter_5/pointers_in_stack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int i = 6;
+	int *p;
+	p = &i;
+	*p = 10;
+	printf("%d \n", i);
+
+	int *q;
+	q = p;
+	*q = 17;
+	printf("%d \n", i);
+	// printf("%p \n", p);
+	// printf("%p \n", q);
+	return 0;
+}
