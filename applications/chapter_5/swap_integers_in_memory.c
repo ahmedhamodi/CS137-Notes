868674bb75d5301dbb85e5098d0d8d2816a2788a
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-10-27 13:55:34 -0400

    Updated version of swapping integers that does not require an additional variable.

--- applications/chapter_5/swap_integers_in_memory.c
+++ applications/chapter_5/swap_integers_in_memory.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+// void swap(int *a, int *b) {
+// 	int temp = *a;
+// 	*a = *b;
+// 	*b = temp;
+// }
 
-int main(void) {
-	int a = 5;
-	int b = 9;
+// int main(void) {
+// 	int a = 5;
+// 	int b = 9;
 	
-	swap(&a, &b);
+// 	swap(&a, &b);
 
-	printf("a: %d\n", a);
-	printf("b: %d\n", b);
-	return 0;
+// 	printf("a: %d\n", a);
+// 	printf("b: %d\n", b);
+// 	return 0;
+// }
+
+//advanced version - no extra variable needed.
+int main(void) {
+	int x = 5;
+	int y = 9;
+	x ^= y;
+	y ^= x;
+	x ^= y;
+	printf("x: %d\n", x);
+	printf("y: %d\n", y);
+	return 0;	
 }
