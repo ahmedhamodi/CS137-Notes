d4bb6a9396fdc92bd0831d3bd5cf0ef6e780df17
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-10-27 14:15:02 -0400

    Added another method of pointing to the largest integer in an array by using pointers and memory addresses.

--- applications/chapter_5/point_to_largest_value.c
+++ applications/chapter_5/point_to_largest_value.c
@@ -4,17 +4,23 @@ int * largest(int a[], int n) {
 	int *ans;
 	if (n < 0) return (int *)0;
 
-	ans = a;
-	for (int *p = a; p < a+n; p++) {
-		if (*p > *ans) ans = p;
-		// printf("%d\n", *ans);
-	}
+	// ans = a;
+	// for (int *p = a; p < a+n; p++) {
+	// 	if (*p > *ans) ans = p;
+	// 	// printf("%d\n", *ans);
+	// }
+	// return ans;
 
-	return ans;
+	//another method
+	int m = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] > a[m]) m = i;
+	}
+	return a + m;
 }
 
 int main(void) {
-	int a[5] = {2, 17, 3, 5, 9};
+	int a[5] = {2, 17, 3, 5, 19};
 	int *p = largest(a, 5);
 	printf("largest: %d\n", *p);
 }
