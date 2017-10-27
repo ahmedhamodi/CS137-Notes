26009cae233cb4384a7f3b1f6c3021b39684cad7
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-10-27 14:26:38 -0400

    Created a C program which shows the logic behind pointer arithmetic.

---
+++ applications/chapter_5/pointer_arithmetic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[8] = {2,3,4,5,6,7,8,9};
+	int *p, *q, i;
+
+	p = &(a[2]);   // p points to a[2]
+	q = p + 3;     // q points to a[5]
+	p += 4;        // p points to a[6]
+	q = q - 2;     // q points to a[3]
+	i = q - p;     // i = 2
+	i = p - q;     // i = -2
+
+	if (p <= q) printf("less\n");
+	else printf("more\n"); //printed
+	
+	return 0;
+}
