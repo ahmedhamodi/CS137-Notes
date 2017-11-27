e0143210dc68835ad6ad209919108daf9e791114
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-11-27 15:43:26 -0500

    First rendition of the fibonacci sequence in C.

---
+++ applications/chapter_9/fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fib(int n) {
+	if (n==0) return 0;
+	if (n==1) return 1;
+	return (fib(n-1)+fib(n-2));
+}
+
+int main(void) {
+	printf("%d\n", fib(10)); //55
+	printf("%d\n", fib(5)); //5
+	printf("%d\n", fib(3)); //2
+	return 1;
+}
