ac8a26e074a1c81621e77c894e30f00677e1120d
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-11-27 15:58:36 -0500

    More efficient fibonacci sequence in C.

---
+++ applications/chapter_9/fibonacci_advanced.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fib(int n) {
+	if (n==0) return 0;
+	int prev = 0, cur = 1;
+	for (int i = 1; i < n; i++) {
+		int next = prev + cur;
+		prev = cur;
+		cur = next;
+	}
+	return cur;
+}
+
+int main(void) {
+	printf("%d\n", fib(10)); //55
+	printf("%d\n", fib(5)); //5
+	printf("%d\n", fib(3)); //2
+	return 1;
+}
