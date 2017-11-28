55b708341038b56d254fb5ea2cb40684f2a0ff2c
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-11-27 19:04:15 -0500

    Removed previous code that was not working.

--- applications/chapter_9/counting_change.c
+++ applications/chapter_9/counting_change.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-// int count(int num, int curNum, int values[], int curIndex, int sum, int numInts) {
-// 	if (curNum == num) return 1;
-// 	if (curNum < num) return 0;
-
-// 	for (int i = 0; i < numInts; i++) {
-// 		sum += count(num, curNum+values[curIndex], values[], curIndex+i);
-// 	}
-// 	return sum;
-// }
-
 int counting_change(int values[], int num, int numInts) {
 	if (num == 0) return 1;
 	if (num < 0) return 0;
