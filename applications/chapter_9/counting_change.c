ab7773cda707ce3e77d5ad02404872bebcbf6350
ahmedhamodi <ahmed.r.hamodi@gmail.com>
2017-11-27 19:03:40 -0500

    Working rendition of the counting change program in C that takes a value, an array of coins, and determines the amount of ways these coins can be grouped to achieve the value given. Coins can be used as many times as needed.

--- applications/chapter_9/counting_change.c
+++ applications/chapter_9/counting_change.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-int count(int num, int curNum, int values[], int curIndex, int sum, int numInts) {
-	if (curNum == num) return 1;
-	if (curNum > num) return 0;
-	for (int i = 0; i < numInts; i++) {
-		sum += count(num, curNum+values[curIndex], values[], curIndex+i);
-	}
-	return sum;
-}
+// int count(int num, int curNum, int values[], int curIndex, int sum, int numInts) {
+// 	if (curNum == num) return 1;
+// 	if (curNum < num) return 0;
+
+// 	for (int i = 0; i < numInts; i++) {
+// 		sum += count(num, curNum+values[curIndex], values[], curIndex+i);
+// 	}
+// 	return sum;
+// }
 
 int counting_change(int values[], int num, int numInts) {
-	int sum = count(num, values[0], values[], 0, 0, numInts);
-	return sum;
+	if (num == 0) return 1;
+	if (num < 0) return 0;
+	if (numInts==0) return 0;
+	return counting_change(values, num-values[numInts-1], numInts) + counting_change(values, num, numInts-1);
 }
 
 int main(void) {
 	int values[] = {5, 10, 15};
 	int num = 20;
 	int numInts = 3;
-	printf("numways: %d\n", counting_change(values[], num, numInts));
+	printf("numways: %d\n", counting_change(values, num, numInts));
+	num = 200;
+	printf("numways: %d\n", counting_change(values, num, numInts));
 }
